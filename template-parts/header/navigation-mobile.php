62b9025dfcea83385eb696f106ba7b536f59ea4e
Lugita Analia Risanty R <lugitarisanty@gmail.com>
2021-03-17 10:53:55 +0700

    html

--- template-parts/header/navigation-mobile.php
+++
@@ -1,7 +0,0 @@
-<?php wp_nav_menu( array(
-	'container' => false,
-	'theme_location' => 'top',
-	'menu_id'        => 'mobile-menu',
-	'menu_class' => 'navbar',
-	'walker' => new wp_bootstrap_navwalker(),
-) ); ?>
